77e14dc5310f7f68963bb0e8668f84dc5f95fc70
강지석 <rkdwltjr5876@naver.com>
2026-04-25 02:31:23 +0900

    chore(api): 메서드 보안 기반 설정 추가
    공통 인가 작업 전에 메서드 단위 보안 어노테이션을 적용할 기반을 준비

--- api/src/main/java/com/ibank/axwms/global/config/SecurityConfig.java
+++ api/src/main/java/com/ibank/axwms/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.ibank.axwms.global.security.SecurityExceptionHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
  */
 @Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
