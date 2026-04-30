edd2585c1a84cd6b62b40151588fcda103d1212d
진경석 <dev.jkjin112@gmail.com>
2026-04-30 17:32:47 +0900

    fix(api): 어노테이션 미스매핑으로 인한 import 수정

--- api/src/main/java/com/ibank/axwms/domain/worklog/controller/InternalWorklogAiCallbackController.java
+++ api/src/main/java/com/ibank/axwms/domain/worklog/controller/InternalWorklogAiCallbackController.java
@@ -3,11 +3,11 @@ package com.ibank.axwms.domain.worklog.controller;
 import com.ibank.axwms.domain.worklog.dto.UpdateWorklogAiApiDto;
 import com.ibank.axwms.domain.worklog.service.InternalWorklogAiCallbackService;
 import com.ibank.axwms.global.response.EmptyResponse;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
