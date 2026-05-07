91af6da3358318ec926381e2c3516e80ab727477
강지석 <rkdwltjr5876@naver.com>
2026-05-07 16:48:59 +0900

    remove(api): 미사용 비밀번호 정책 클래스 삭제
    비어 있는 PasswordPolicy 클래스와 불필요한 policy 패키지 placeholder 제거

--- api/src/main/java/com/ibank/axwms/domain/auth/policy/PasswordPolicy.java
+++
@@ -1,9 +0,0 @@
-package com.ibank.axwms.domain.auth.policy;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-public class PasswordPolicy {
-}
